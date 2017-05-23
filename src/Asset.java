e1764682e65b97a747aedc7dd65d9df71dce9008
Daniel Graaf <Dan@dyn1193-147.wlan.ic.ac.uk>
2017-05-23 14:34:39 +0100

    made it easier to add mutiple assets

--- src/Asset.java
+++ src/Asset.java
@@ -1,9 +1,92 @@
+import java.util.Random;
+
 /**
  * Created by Dan on 22/05/2017.
  */
 public class Asset {
 
-    public Asset(double drift, double volatility, double start_price) {
+    private double[] prices;
+    private double[] norms;
+    private double[] weights;
+    private double drift;
+    private double volatility;
+    private Asset untraded;
 
+    public Asset(double drift, double volatility, double start_price, int num_to_simulate, double DT) {
+        this.drift = drift;
+        this.volatility = volatility;
+        this.prices = new double[num_to_simulate];
+        this.norms = new double[num_to_simulate];
+        this.weights = new double[num_to_simulate];
+        setNorms();
+        setPrices(start_price, DT);
     }
+
+    public Asset(Asset untraded, double drift, double volatility, double start_price, int num_to_simulate, double DT, double RHO) {
+        this.untraded = untraded;
+        this.drift = drift;
+        this.volatility = volatility;
+        this.prices = new double[num_to_simulate];
+        this.norms = new double[num_to_simulate];
+        this.weights = new double[num_to_simulate];
+        setNorms(RHO);
+        setPrices(start_price, DT);
+    }
+
+    private void setPrices(double start_price, double DT) {
+        this.prices[0] = start_price;
+        for (int i = 1; i < this.prices.length; i++) {
+            this.prices[i] = this.prices[i - 1] *
+                    Math.exp(
+                            (this.drift - 0.5 * this.volatility * this.volatility) * (DT)
+                                    + this.volatility * Math.sqrt(DT) * this.norms[i-1]
+                    );
+        }
+    }
+
+    private void setNorms() {
+        Random random = new Random();
+        for (int i = 0; i < this.norms.length; i++) {
+            this.norms[i] = random.nextGaussian();
+        }
+    }
+
+    private void setNorms(double RHO) {
+        Random random = new Random();
+        for (int i = 0; i < this.norms.length; i++) {
+            this.norms[i] = RHO * this.untraded.getNorms()[i] + Math.sqrt(1 - RHO) * random.nextGaussian();
+        }
+    }
+
+    public double[] getNorms() {
+        return this.norms;
+    }
+
+    public double[] getPrices() {
+        return this.prices;
+    }
+
+    public double getDrift() {
+        return this.drift;
+    }
+
+    public double getVolatility() {
+        return this.volatility;
+    }
+
+    public void setWeight(int time_period, double weight) {
+        this.weights[time_period] = weight;
+    }
+
+    public void setWeightAll(double weight) {
+        for (int i = 0; i < this.weights.length; i ++) {
+            this.weights[i] = weight;
+        }
+    }
+
+    public double getWeight(int time_period) {
+        return this.weights[time_period];
+    }
+
+
 }
--- src/OptionPricer.java
+++ src/OptionPricer.java
@@ -10,28 +10,17 @@ import java.util.*;
  */
 public class OptionPricer {
 
-    Map<Integer, Asset> assets = new HashMap<>();
-    private double muCorrelated;
-    private double sigmaCorrelated;
-    private double muUntraded;
-    private double sigmaUntraded;
-    private double startPriceCorrelated;
-    private double startPriceUntraded;
+    private Asset untraded;
+    ArrayList<Asset> assets = new ArrayList<>();
     private int numDays;
     private int hedgesPerDay;
-    private double[] untradedPrices;
-    private double[] correlatedPrices;
     private double[] dzc;
     private double[] dze;
-    private static double[] NORMS1;
-    private static double[] NORMS2;
     private static double DT;
     private static double INTEREST = 0.05;
-    private static double STRIKE = 300;
-    private static double RHO = 0.8;
+    private static double STRIKE = 250;
     private double w;
     private double bec;
-    private boolean hedge;
     private double T;
 
     public OptionPricer(String[] args, int n) {
@@ -44,44 +33,46 @@ public class OptionPricer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < Integer.parseInt(args[1]); i++) {
-            this.assets.put(i, new Asset(
-                    Double.parseDouble(prop.getProperty("correlated_mu_" + i)),
-                    Double.parseDouble(prop.getProperty("correlated_vol_" + i)),
-                    Double.parseDouble(prop.getProperty("start_price_correlated_" + i))
-            ));
-        }
-        //this.muCorrelated = Double.parseDouble(prop.getProperty("correlated_mu"));
-        //this.sigmaCorrelated = Double.parseDouble(prop.getProperty("correlated_vol"));
-        this.muUntraded = Double.parseDouble(prop.getProperty("untraded_mu"));
-        this.sigmaUntraded = Double.parseDouble(prop.getProperty("untraded_vol"));
-        this.startPriceUntraded = Double.parseDouble(prop.getProperty("start_price_untraded"));
-        //this.startPriceCorrelated = Double.parseDouble(prop.getProperty("start_price_correlated"));
         this.numDays = Integer.parseInt(prop.getProperty("num_days"));
         this.hedgesPerDay = Integer.parseInt(prop.getProperty("hedges_per_day"));
         this.T = this.numDays / 365.0;
         DT = (this.T) / (this.numDays * this.hedgesPerDay);
-        NORMS1 = new double[this.hedgesPerDay * this.numDays];
-        NORMS2 = new double[this.hedgesPerDay * this.numDays];
-        try {
-            makeRandomNormals();
-        } catch (Exception e) {
-            System.out.println("here");
+        this.untraded = new Asset(
+                Double.parseDouble(prop.getProperty("untraded_mu")),
+                Double.parseDouble(prop.getProperty("untraded_vol")),
+                Double.parseDouble(prop.getProperty("start_price_untraded")),
+                this.numDays * this.hedgesPerDay,
+                DT
+        );
+        for (int i = 1; i < Integer.parseInt(args[1]); i++) {
+            this.assets.add(new Asset(
+                    this.untraded,
+                    Double.parseDouble(prop.getProperty("correlated_mu_" + i)),
+                    Double.parseDouble(prop.getProperty("correlated_vol_" + i)),
+                    Double.parseDouble(prop.getProperty("start_price_correlated_" + i)),
+                    this.numDays * this.hedgesPerDay,
+                    DT,
+                    Double.parseDouble(prop.getProperty("RHO_" + i))
+            ));
         }
-        setUntradedPrices();
-        setCorrelatedPrices();
+        this.untraded.setWeightAll(1);
+        setWeightsAll(); // should be replaced
         this.bec = bec();
-        this.w = this.muUntraded - this.bec*(this.muCorrelated - INTEREST);
+        //this.w = this.muUntraded - this.bec*(this.muCorrelated - INTEREST);
         //printPrices(n);
     }
 
     public double calculateOptionPrice(int day) {
-        double d1 = (Math.log(this.untradedPrices[day] / STRIKE) + (this.w + (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - day * DT))
-                / (this.sigmaUntraded * Math.sqrt(this.T - day * DT));
-        double d2 = (Math.log(this.untradedPrices[day] / STRIKE) + (this.w - (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - day * DT))
-                / (this.sigmaUntraded * Math.sqrt(this.T - day * DT));
+        //double d1 = (Math.log(this.untradedPrices[day] / STRIKE) + (this.w + (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - day * DT))
+        //        / (this.sigmaUntraded * Math.sqrt(this.T - day * DT));
+        //double d2 = (Math.log(this.untradedPrices[day] / STRIKE) + (this.w - (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - day * DT))
+        //        / (this.sigmaUntraded * Math.sqrt(this.T - day * DT));
+        double d1 = (Math.log(this.untraded.getPrices()[day] / STRIKE) + (this.w + (0.5) * this.untraded.getVolatility() * this.untraded.getVolatility()) * (this.T - day * DT))
+                  / (this.untraded.getVolatility() * Math.sqrt(this.T - day * DT));
+        double d2 = (Math.log(this.untraded.getPrices()[day] / STRIKE) + (this.w - (0.5) * this.untraded.getVolatility() * this.untraded.getVolatility()) * (this.T - day * DT))
+                / (this.untraded.getVolatility() * Math.sqrt(this.T - day * DT));
         NormalDistribution distribution = new NormalDistribution(0, 1);
-        double V = this.startPriceUntraded * Math.exp((w - INTEREST) * (this.T - day * DT)) * distribution.cumulativeProbability(d1)
+        double V = this.untraded.getPrices()[0] * Math.exp((this.w - INTEREST) * (this.T - day * DT)) * distribution.cumulativeProbability(d1)
                 - STRIKE * Math.exp(-1 * INTEREST * (this.T - day * DT)) * distribution.cumulativeProbability(d2);
         return V;
     }
@@ -98,21 +89,27 @@ public class OptionPricer {
         //Option value : 15
         //option expires at 180 with strike 200.
         //profit = 15 + 0 = 15.
-        return calculateOptionPrice(0) * Math.exp(INTEREST * this.T) - Math.max(0, this.untradedPrices[this.untradedPrices.length - 1] - STRIKE);
+        this.setWeightsAll();
+        //return calculateOptionPrice(0) * Math.exp(INTEREST * this.T) - Math.max(0, this.untradedPrices[this.untradedPrices.length - 1] - STRIKE);
+        return 10;
     }
 
     public double calculateProfitSingleHedge() {
         //We take (G - phi) and invest it in the risk free.
         //We take phi and invest it in the correlated asset at time t = 0.
         //We calculate the final profit.
-        double d1 = (Math.log(this.untradedPrices[0] / STRIKE) + (this.w + (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - 0 * DT))
-                / (this.sigmaUntraded * Math.sqrt(this.T - 0 * DT));
+        this.setWeightsAll();
+        this.w = this.untraded.getDrift() - this.bec * (this.calculatePortfolioMu(0) - INTEREST);
+        double d1 = (Math.log(this.untraded.getPrices()[0] / STRIKE) + (this.w + (0.5) * this.untraded.getVolatility() * this.untraded.getVolatility()) * (this.T - 0 * DT))
+                / (this.untraded.getVolatility() * Math.sqrt(this.T - 0 * DT));
         NormalDistribution norm = new NormalDistribution(0, 1);
-        double phi = this.startPriceUntraded * Math.exp((this.w - INTEREST) * (this.T - 0 * DT)) * norm.cumulativeProbability(d1) * this.bec;
+        double phi = this.untraded.getPrices()[0] * Math.exp((this.w - INTEREST) * (this.T - 0 * DT)) * norm.cumulativeProbability(d1) * this.bec;
         double G = this.calculateOptionPrice(0);
         double riskFree = G - phi;
-        double moneyFromCorrelated = (phi / this.correlatedPrices[0]) * this.correlatedPrices[this.correlatedPrices.length - 1];
-        return -1 * Math.max(0, this.untradedPrices[this.untradedPrices.length - 1] - STRIKE) + moneyFromCorrelated + riskFree * Math.exp(INTEREST * this.T);
+        //double moneyFromCorrelated = (phi / this.correlatedPrices[0]) * this.correlatedPrices[this.correlatedPrices.length - 1];
+        double moneyFromCorrelated = (phi / calculatePortfolioValue(0)) * calculatePortfolioValue(this.hedgesPerDay * this.numDays - 1);
+        //return -1 * Math.max(0, this.untradedPrices[this.untradedPrices.length - 1] - STRIKE) + moneyFromCorrelated + riskFree * Math.exp(INTEREST * this.T);
+        return -1 * Math.max(0, this.untraded.getPrices()[this.untraded.getPrices().length - 1] - STRIKE) + moneyFromCorrelated + riskFree * Math.exp(INTEREST * this.T);
     }
 
     public double calculateProfitMultiHedge() {
@@ -122,73 +119,44 @@ public class OptionPricer {
         double profitFromCorrelated = 0;
         double costOfBorrowing = 0;
         double G = this.calculateOptionPrice(0);
+        this.w = this.untraded.getDrift() - this.bec * (this.calculatePortfolioMu(0) - INTEREST);
         NormalDistribution norm = new NormalDistribution(0, 1);
-        double d1 = (Math.log(this.untradedPrices[0] / STRIKE) + (this.w + (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - 0 * DT))
-                / (this.sigmaUntraded * Math.sqrt(this.T - 0 * DT));
-        double phiPrev = this.startPriceUntraded * Math.exp((this.w - INTEREST) * (this.T - 0 * DT)) * norm.cumulativeProbability(d1) * this.bec;
+        double d1 = (Math.log(this.untraded.getPrices()[0] / STRIKE) + (this.w + (0.5) * this.untraded.getVolatility() * this.untraded.getVolatility()) * (this.T - 0 * DT))
+                / (this.untraded.getVolatility() * Math.sqrt(this.T - 0 * DT));
+        double phiPrev = this.untraded.getPrices()[0] * Math.exp((this.w - INTEREST) * (this.T - 0 * DT)) * norm.cumulativeProbability(d1) * this.bec;
         double riskFree = 0;
         for (int i = 1; i < this.hedgesPerDay * this.numDays; i++) {
             //Get the ith d1.
-            d1 = (Math.log(this.untradedPrices[i] / STRIKE) + (this.w + (0.5) * this.sigmaUntraded * this.sigmaUntraded) * (this.T - i * DT))
-                    / (this.sigmaUntraded * Math.sqrt(this.T - i * DT));
+            this.w = this.untraded.getDrift() - this.bec * (this.calculatePortfolioMu(i) - INTEREST);
+            d1 = (Math.log(this.untraded.getPrices()[i] / STRIKE) + (this.w + (0.5) * this.untraded.getVolatility() * this.untraded.getVolatility()) * (this.T - i * DT))
+                    / (this.untraded.getVolatility() * Math.sqrt(this.T - i * DT));
             //Get the ith phi.
-            double phi = this.untradedPrices[i] * Math.exp((this.w - INTEREST) * (this.T - i * DT)) * norm.cumulativeProbability(d1) * this.bec;
-            double dG = ((G - phi) * INTEREST + phi * this.muCorrelated) * DT + phi * this.sigmaCorrelated * this.dzc[i];
+            double phi = this.untraded.getPrices()[i] * Math.exp((this.w - INTEREST) * (this.T - i * DT)) * norm.cumulativeProbability(d1) * this.bec;
+            double dG = ((G - phi) * INTEREST + phi * this.calculatePortfolioMu(i)) * DT + phi * this.calculatePortfolioSigma(i) * this.dzc[i];
             riskFree = G - phi;
             G += dG;
-            profitFromCorrelated += (((phiPrev / this.correlatedPrices[i - 1]) * this.correlatedPrices[i]) - phiPrev);
+            profitFromCorrelated += (((phiPrev / this.calculatePortfolioValue(i - 1)) * this.calculatePortfolioValue(i)) - phiPrev);
             //System.out.println("phi: " + phi);
             //System.out.println("Corr[" + i + "]: " + this.correlatedPrices[i]);
             //System.out.println("phiPrev: " + phiPrev);
             //System.out.println("Corr[" + (i - 1) + "]: " + this.correlatedPrices[i - 1]);
             //System.out.println("Prof: " + (((phiPrev / this.correlatedPrices[i - 1]) * this.correlatedPrices[i]) - phiPrev));
             //System.out.println();
+            System.out.println("V: " + calculateOptionPrice(i));
             costOfBorrowing += (riskFree * INTEREST * DT);
             phiPrev = phi;
         }
-//        System.out.println("V: " + calculateOptionPrice(0));
-//        System.out.println("payout: " + -Math.max(0, this.untradedPrices[this.untradedPrices.length - 1] - STRIKE));
-//        System.out.println("prof: " + profitFromCorrelated);
-//        System.out.println("Cost: " + costOfBorrowing);
-//        System.out.println("G: " + G);
-        double profit = - (1 * Math.max(0, this.untradedPrices[this.untradedPrices.length - 1] - STRIKE)) + profitFromCorrelated + calculateOptionPrice(0) + costOfBorrowing;
-//        System.out.println(profit);
-//        System.out.println();
+        System.out.println("V: " + calculateOptionPrice(0));
+        System.out.println("payout: " + -Math.max(0, this.untraded.getPrices()[this.untraded.getPrices().length - 1] - STRIKE));
+        System.out.println("prof: " + profitFromCorrelated);
+        System.out.println("Cost: " + costOfBorrowing);
+        System.out.println("G: " + G);
+        double profit = - (1 * Math.max(0, this.untraded.getPrices()[this.untraded.getPrices().length - 1] - STRIKE)) + profitFromCorrelated + calculateOptionPrice(0) + costOfBorrowing;
+        System.out.println(profit);
+        System.out.println();
         return profit;
     }
 
-    private void makeRandomNormals() {
-        Random random = new Random();
-            for (int i = 0; i < this.numDays * this.hedgesPerDay; i ++) {
-                NORMS1[i] = random.nextGaussian();
-                NORMS2[i] = RHO * NORMS1[i] + Math.sqrt(1 - RHO) * random.nextGaussian();
-            }
-    }
-
-
-    private void setUntradedPrices() {
-        this.untradedPrices = new double[this.numDays * this.hedgesPerDay];
-        this.untradedPrices[0] = this.startPriceUntraded;
-        for (int i = 1; i < this.untradedPrices.length; i++) {
-            this.untradedPrices[i] = this.untradedPrices[i - 1] *
-                    Math.exp(
-                            (this.muUntraded - 0.5 * this.sigmaUntraded * this.sigmaUntraded) * (DT)
-                                    + this.sigmaUntraded * Math.sqrt(DT) * NORMS1[i-1]
-                    );
-        }
-    }
-
-    private void setCorrelatedPrices() {
-        this.correlatedPrices = new double[this.numDays * this.hedgesPerDay];
-        this.correlatedPrices[0] = this.startPriceCorrelated;
-        for (int i = 1; i < this.correlatedPrices.length; i++) {
-            this.correlatedPrices[i] = this.correlatedPrices[i - 1] *
-                    Math.exp(
-                            (this.muCorrelated - 0.5 * this.sigmaCorrelated * this.sigmaCorrelated) * (DT)
-                                    + this.sigmaCorrelated * Math.sqrt(DT) * NORMS2[i-1]
-                    );
-        }
-    }
 
     public void printPrices(int n) {
         try {
@@ -196,9 +164,9 @@ public class OptionPricer {
             PrintWriter correlatedWriter = new PrintWriter("correlated-prices" + n + ".txt", "UTF-8");
             for (int i = 0; i < this.numDays * this.hedgesPerDay; i++) {
                 untradedWriter.print(i + " ");
-                untradedWriter.println(this.untradedPrices[i] + " ");
+                //untradedWriter.println(this.untradedPrices[i] + " ");
                 correlatedWriter.print(i + " ");
-                correlatedWriter.println(this.correlatedPrices[i] + " ");
+                //correlatedWriter.println(this.correlatedPrices[i] + " ");
             }
             untradedWriter.close();
             correlatedWriter.close();
@@ -211,12 +179,59 @@ public class OptionPricer {
     public double bec() {
         this.dze = new double[this.numDays * this.hedgesPerDay];
         this.dzc = new double[this.numDays * this.hedgesPerDay];
+        double portfolioValuePrev = calculatePortfolioValue(0);
         for (int i = 1; i < this.numDays * this.hedgesPerDay; i++) {
-            this.dze[i] = (((this.untradedPrices[i] - this.untradedPrices[i-1]) - this.muUntraded * this.untradedPrices[i] * DT)/ (this.sigmaUntraded * this.untradedPrices[i]));
-            this.dzc[i] = (((this.correlatedPrices[i] - this.correlatedPrices[i-1]) - this.muCorrelated * this.correlatedPrices[i] * DT)/ (this.sigmaCorrelated * this.correlatedPrices[i]));
+            this.dze[i] = (((this.untraded.getPrices()[i] - this.untraded.getPrices()[i-1]) - this.untraded.getDrift() * this.untraded.getPrices()[i] * DT)/ (this.untraded.getVolatility() * this.untraded.getPrices()[i]));
+            this.dzc[i] = (((calculatePortfolioValue(i) - portfolioValuePrev) - this.calculatePortfolioMu(i) * this.calculatePortfolioValue(i) * DT)/ (this.calculatePortfolioSigma(i) * this.calculatePortfolioValue(i)));
+            portfolioValuePrev = calculatePortfolioValue(i);
         }
         Covariance cov = new Covariance();
         double pec = cov.covariance(this.dze, this.dzc) / StatUtils.variance(this.dzc);
-        return pec * this.sigmaUntraded / this.sigmaCorrelated;
+        return pec * this.untraded.getVolatility() / this.calculatePortfolioSigma(this.dze.length - 1);
+    }
+
+    private double calculatePortfolioMu(int i) {
+        double mu = 0;
+        for (Asset asset : this.assets) {
+            // don't include the untraded.
+            mu += asset.getWeight(i) * asset.getDrift();
+        }
+        return mu;
+    }
+
+    private double calculatePortfolioSigma(int i) {
+        double sigma = 0;
+        for (Asset asset : this.assets) {
+            // don't include the untraded.
+            sigma += asset.getWeight(i) * asset.getVolatility();
+        }
+        return sigma;
+    }
+
+    private void setWeights(int time_period) {
+        // check it sets to 1 when two assets etc.
+        int num_assets = this.assets.size();
+        for (Asset asset : this.assets) {
+            asset.setWeightAll(1 / (num_assets - 1.0));
+        }
+    }
+
+    private void setWeightsAll() {
+        // check it sets to 1 when two assets etc.
+        for (Asset asset : this.assets) {
+            asset.setWeightAll(1 / this.assets.size());
+        }
+    }
+
+    private double calculateWeight() {
+        return 0;
+    }
+
+    private double calculatePortfolioValue(int i) {
+        double value = 0;
+        for (Asset asset : this.assets) {
+            value += asset.getWeight(i) * asset.getPrices()[i];
+        }
+        return value;
     }
 }
