d7d353391e75751a84eb74d95c6efc74e03638c3
Daniel Graaf <dg1613@ic.ac.uk>
2017-06-04 09:42:46 +0100

    switching comp

--- binomial-pricer.py
+++ binomial-pricer.py
@@ -12,34 +12,39 @@ ANNUAL_INTEREST = (float(sys.argv[4]))
 INTEREST = 1 + (ANNUAL_INTEREST / (1.0 / DT))
 start_price = float(sys.argv[5])
 STRIKE = float(sys.argv[6])
-num_assets = 3
+num_assets = 2
 
 u = np.exp(sigma * np.sqrt(DT))
 d = np.exp((-1) * sigma * np.sqrt(DT))
 end_prices = np.zeros(DTs+1)
 
-start_prices_assets = [5, 6, 1]
-assets_sigma = [0.32, 0.12, 0]
+start_prices_assets = [5, 3]
+assets_sigma = [0.32, 0.22]
 asset_us = np.zeros(num_assets)
 asset_ds = np.zeros(num_assets)
 end_prices_assets = np.zeros(shape=(num_assets, DTs + 1))
 
 print_whole_tree = True
+with_bond = False
 
 
 def set_us():
     global asset_us
     for i in range(len(asset_us) - 1):
-        # asset_us[i] = np.exp(assets_sigma[i] * np.sqrt(DT))
-        asset_us[i] = 1.1
-    asset_us[len(asset_us) - 1] = INTEREST
+        asset_us[i] = np.exp(assets_sigma[i] * np.sqrt(DT))
+    if (with_bond):
+        asset_us[len(asset_us) - 1] = INTEREST
+    else:
+        asset_us[len(asset_us) - 1] = np.exp(assets_sigma[len(asset_us) - 1] * np.sqrt(DT))
 
 def set_ds():
     global asset_ds
     for i in range(len(asset_ds) - 1):
-        # asset_ds[i] = np.exp((-1) * assets_sigma[i] * np.sqrt(DT))
-        asset_ds[i] = 1.0/1.1
-    asset_ds[len(asset_ds) - 1] = INTEREST
+        asset_ds[i] = np.exp((-1) * assets_sigma[i] * np.sqrt(DT))
+    if (with_bond):
+        asset_ds[len(asset_ds) - 1] = INTEREST
+    else:
+        asset_ds[len(asset_us) - 1] = np.exp((-1) * assets_sigma[len(asset_us) - 1] * np.sqrt(DT))
 
 def find_value_optimal(end_prices, day):
     # end_prices[1] = Cu
@@ -68,9 +73,13 @@ def set_finals_assets():
         for j in range(len(end_prices_assets[i])):
             end_prices_assets[i][j] = start_prices_assets[i] * (pow(asset_us[i], j + 1)) * (pow(asset_ds[i], len(end_prices_assets[i]) - j))
     for j in range(len(end_prices_assets[len(end_prices_assets) - 1])):
-        end_prices_assets[len(end_prices_assets) - 1][j] = find_bond_value(DTs)
+        if (with_bond):
+            end_prices_assets[len(end_prices_assets) - 1][j] = find_bond_value(DTs)
+        else:
+            end_prices_assets[len(end_prices_assets) - 1][j] = start_prices_assets[len(end_prices_assets) - 1] * (pow(asset_us[len(end_prices_assets) - 1], j + 1)) * (pow(asset_ds[len(end_prices_assets) - 1], len(end_prices_assets[len(end_prices_assets) - 1]) - j))
 
 def find_bond_value(t):
+    # print("bond value at t: " + str(t) + " = " + str(start_prices_assets[len(start_prices_assets) - 1] * pow(asset_us[len(asset_us) - 1], t)))
     return start_prices_assets[len(start_prices_assets) - 1] * pow(asset_us[len(asset_us) - 1], t)
 
 def find_value_incomplete(end_prices_assets, end_prices, t):
@@ -97,11 +106,13 @@ def find_value_incomplete(end_prices_assets, end_prices, t):
             temp_end_prices = [end_prices[i], end_prices[i+1]]
             for j in range(len(temp_asset_prices) - 1):
                 prev_end_prices_assets[j][i] = temp_asset_prices[j][0] * asset_us[j]
-            prev_end_prices_assets[len(temp_asset_prices) - 1] = find_bond_value(t - 1)
+            if (with_bond):
+                prev_end_prices_assets[len(temp_asset_prices) - 1] = find_bond_value(t - 1)
+            else:
+                prev_end_prices_assets[len(temp_asset_prices) - 1] = temp_asset_prices[len(temp_asset_prices) - 1][0] * asset_us[len(temp_asset_prices) - 1]
+                # print("prev_end_prices_assets : " + str(prev_end_prices_assets))
             price = (calc_incomplete_price(temp_asset_prices, temp_end_prices, (t - 1)))
             prev_end_prices[i] = price
-        # print(prev_end_prices)
-        # print(prev_end_prices_assets)
         # print("--------------")
         return find_value_incomplete(prev_end_prices_assets, prev_end_prices, (t-1))
 
@@ -123,11 +134,23 @@ def calc_incomplete_price(end_prices_assets, end_prices, day):
         b[i] = perms[i][len(perms[i]) - 1]
 
     vals = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(A), A)), np.transpose(A)),np.transpose(b))
-    # print ("decisions: ", vals)
+    diff = 0
     value = 0
+    # print ("vals: ", vals)
+    for perm in perms:
+        total = 0
+        for asset in range(len(vals)):
+            total += vals[asset] * perm[asset]
+        diff = max(abs(perm[len(perms[0]) - 1] - total), diff)
+    print ("Diff : " + str(diff))
     for i in range(len(vals) - 1):
+        # print("multiplying: " + str(end_prices_assets[i][0] * asset_us[i]) + " and " + str(vals[i]) + " = " + str(end_prices_assets[i][0] * asset_us[i] * vals[i]))
         value += end_prices_assets[i][0] * asset_us[i] * vals[i]
-    value += vals[len(vals) - 1] * find_bond_value(day - 1)
+    if (with_bond):
+        value += vals[len(vals) - 1] * find_bond_value(day - 1)
+    else:
+        # print("multiplying: " + str(end_prices_assets[len(vals) - 1][0] * asset_us[len(vals) - 1]) + " and " + str(vals[len(vals) - 1]) + " = " + str(end_prices_assets[len(vals) - 1][0] * asset_us[len(vals) - 1] * vals[len(vals) - 1]))
+        value += vals[len(vals) - 1] * end_prices_assets[len(vals) - 1][0] * asset_us[len(vals) - 1]
     return value
 
 
@@ -136,6 +159,6 @@ set_us()
 set_ds()
 set_finals_assets()
 val = find_value_incomplete(end_prices_assets, end_prices, DTs)
-print ("our val: " + str(val))
+print (val)
 val = find_value_optimal(end_prices, DTs)
-print ("optimal val: " + str(val))
+print (val)
