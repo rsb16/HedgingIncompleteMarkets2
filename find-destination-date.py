054856c505569c6e92ccc6334183796c6f3114c9
Daniel Graaf <dg1613@ic.ac.uk>
2017-06-11 11:08:41 +0100

    fixed script to give the prices per departure date

--- find-destination-date.py
+++ find-destination-date.py
@@ -2,20 +2,19 @@
 
 import json
 import sys
-
-with open('flight-data.json.backup', 'r') as f:
-    current_data = json.loads(f.read())
-
 destination = unicode(sys.argv[1])
 date = unicode(sys.argv[2])
 
+with open('flight-data-{}-test.json'.format(destination), 'r') as f:
+    current_data = json.loads(f.read())
+
 filtered_data = filter(
     lambda x:   'results' in x and
-                x['results'][0]['destination'] == destination and
-                x['results'][0]['departure_date'] == date,
+                x['results']['destination'] == destination and
+                x['results']['departure_date'] == date,
     current_data
 )
 
-mapped_data = map(lambda x: x['results'][0]['price'], filtered_data)
+mapped_data = map(lambda x: x['results']['price'], filtered_data)
 
 print(json.dumps(mapped_data, indent=2))
