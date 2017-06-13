a310a9e015947f4ad5a700f1babc91111ebf8ea2
Daniel Graaf <dg1613@ic.ac.uk>
2017-06-13 08:21:28 +0100

    plt.py

--- plot.py
+++ plot.py
@@ -6,14 +6,15 @@ av_g = []
 av_d = []
 
 def main():
-    for i in range(1, 100):
-        with open('result-data-' + str(i) + '.txt') as f:
+    for i in range(1, 80):
+        with open('./data/result-data-' + str(i) + '.txt') as f:
             data = f.read().split('\n')
+            print data[0]
             denault.append(float(data[0]))
             graaf.append(float(data[1]))
     plt.plot(graaf, label='mcls')
     plt.plot(denault, label='den')
-    for i in range(1, 100):
+    for i in range(1, 80):
         av_g.append(sum(graaf)/len(graaf))
         av_d.append(sum(denault)/len(denault))
     plt.plot(av_g, label='av_mcls')
